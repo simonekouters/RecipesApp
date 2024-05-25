d6b765f1f178c1cc7975bcde9286a46c5978d9ed
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-05-25 21:00:13 +0200

    Add exceptions and advice

---
+++ backend/src/main/java/com/simonekouters/recipes/exception/Advice.java
@@ -0,0 +1,21 @@
+package com.simonekouters.recipes.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class Advice {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ProblemDetail> badRequestHandler(BadRequestException exception) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
+        return ResponseEntity.badRequest().body(problemDetail);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Void> notFoundHandler(NotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+}
---
+++ backend/src/main/java/com/simonekouters/recipes/exception/NotFoundException.java
@@ -0,0 +1,4 @@
+package com.simonekouters.recipes.exception;
+
+public class NotFoundException extends RuntimeException {
+}
