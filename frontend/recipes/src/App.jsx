44ed8abe652e3b0904bcfea113be767001b2e954
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-05-25 18:07:45 +0200

    Delete unnecessary component in between App and RecipeForm

--- frontend/recipes/src/App.jsx
+++ frontend/recipes/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import './App.css';
 import RecipesList from './components/RecipesList/RecipesList';
 import Recipe from './components/Recipe/Recipe';
-import AddRecipe from './components/AddRecipe/AddRecipe';
 import { Routes, Route, useNavigate } from "react-router-dom";
+import RecipeForm from './components/RecipeForm/RecipeForm';
 
 function App() {
   const API_URL = "http://localhost:8080/recipes";
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<RecipesList navigate={navigate} API_URL={API_URL} recipes={recipes} setRecipes={setRecipes} />} />
         <Route path="/recipes/:id" element={<Recipe />} />
-        <Route path="/add-recipe" element={<AddRecipe API_URL={API_URL} recipes={recipes} setRecipes={setRecipes} />} />
+        <Route path="/add-recipe" element={<RecipeForm API_URL={API_URL} recipes={recipes} setRecipes={setRecipes} />} />
       </Routes>
     </div>
   )
