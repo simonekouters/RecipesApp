2c6e3e65c17f794234d4f9ca528991d106580613
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-05-25 17:32:21 +0200

    Fetching and rendering the entire recipe in one component

--- frontend/recipes/src/components/Recipe/Recipe.jsx
+++ frontend/recipes/src/components/Recipe/Recipe.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
-import IngredientsList from '../Ingredients/IngredientsList';
-import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
-import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import axios from 'axios'
 
 function Recipe() {
-  const [recipe, setRecipe] = useState(null);
+  const [title, setTitle] = useState("");
   const [ingredients, setIngredients] = useState([]);
+  const [steps, setSteps] = useState([]);
   const { id } = useParams();
   const API_URL = `http://localhost:8080/recipes/${id}`;
 
@@ -15,25 +15,40 @@ function Recipe() {
       try {
         const response = await axios(API_URL);
         const data = response.data;
-        setRecipe(data);
+        if (data) {
+          setTitle(data.title || "");
+          setIngredients(data.ingredients || []);
+          setSteps(data.steps || []);
+          
+        }
       } catch (error) {
-        console.error("Error fetching recipe: ", error)
+        console.error("Error fetching data: ", error)
       }
     }
     getData();
   }, [API_URL]);
 
+
   return (
     <>
-      {recipe && (
-        <>
-          <h2>{recipe.title}</h2>
-          <h3>Ingredients</h3>
-          <IngredientsList ingredients={ingredients} setIngredients={setIngredients} API_URL={API_URL} />
-        </>
-      )}  
+      <h2>{title}</h2>
+
+      <h3>Ingredients</h3>
+      <ul>
+        {ingredients.map(ingredient => {
+          const formattedIngredient = `${ingredient.ingredient.name} - ${ingredient.quantity} ${ingredient.unit}`;
+          return <li key={ingredient.id}>{formattedIngredient}</li>;
+        })}
+      </ul>
+
+      <h3>Method</h3>
+      <ul>
+        {steps.map((step, index) => (
+          <li key={index}>{`Step ${index + 1} - ${step}`}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
 export default Recipe;
