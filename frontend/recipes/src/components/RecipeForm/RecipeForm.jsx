82ff2c47e29cc99c503a6f36854540e57500bcfb
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-05-26 17:17:59 +0200

    Refactor RecipeForm component with custom hook

--- frontend/recipes/src/components/RecipeForm/RecipeForm.jsx
+++ frontend/recipes/src/components/RecipeForm/RecipeForm.jsx
@@ -1,74 +1,12 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+import React from 'react';
 import AddedIngredients from './AddedIngredients';
 import AddedSteps from './AddedSteps';
+import useRecipeForm from './useRecipeForm';
 
 function RecipeForm({ recipes, setRecipes, API_URL, navigate }) {
-  const [recipe, setRecipe] = useState({ title: "", ingredients: [], steps: [] });
-  const [newIngredient, setNewIngredient] = useState({ ingredient: { name: "" }, unit: "", quantity: "" });
-  const [newStep, setNewStep] = useState("");
-
-  function handleAddIngredient(e) {
-    e.preventDefault();
-    if (!newIngredient.ingredient.name.trim()) {
-      alert("Ingredient name can't be empty");
-      return;
-    }
-    if (!newIngredient.quantity.trim()) {
-      alert("Quantity can't be empty");
-      return;
-    }
-    if (isNaN(newIngredient.quantity)) {
-      alert("Quantity should be a number")
-      return;
-    }
-
-    const quantityValue = parseInt(newIngredient.quantity, 10);
-    const ingredientToAdd = { ...newIngredient, quantity: quantityValue };
-    setRecipe({ ...recipe, ingredients: [...recipe.ingredients, ingredientToAdd] });
-    setNewIngredient({ ingredient: { name: "" }, unit: "", quantity: "" });
-  }
-
-  function handleAddStep(e) {
-    e.preventDefault();
-    if (!newStep.trim()) {
-      alert("Step can't be empty");
-      return;
-    }
-    if (newStep.length >= 255) {
-      alert("A step can't be more than 255 characters long")
-      return;
-    }
-    setRecipe({ ...recipe, steps: [...recipe.steps, newStep] });
-    setNewStep("");
-  }
-
-  async function handleSaveRecipe(e) {
-    e.preventDefault();
-    if (!recipe.title.trim()) {
-      alert("Title can't be empty");
-      return;
-    }
-    if (recipe.ingredients.length === 0) {
-      alert("Ingredients can't be empty");
-      return;
-    }
-    if (recipe.steps.length === 0) {
-      alert("Method can't be empty");
-      return;
-    }
-
-    try {
-      const response = await axios.post(API_URL, { title: recipe.title, steps: recipe.steps, ingredients: recipe.ingredients });
-      const newRecipe = response.data;
-      setRecipes([...recipes, newRecipe]);
-      setRecipe({ title: "", ingredients: [], steps: [] });
-      navigate("/");
-    } catch (error) {
-      console.error("Error adding recipe: ", error);
-    }
-  }
-
+  const { recipe, setRecipe, newIngredient, newStep, setNewIngredient, setNewStep, handleAddIngredient,
+    handleAddStep, handleSaveRecipe } = useRecipeForm({ API_URL, recipes, setRecipes, navigate });
+ 
   return (
     <>
       <h2>Add a recipe</h2>
